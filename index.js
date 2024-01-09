e322e5fcf1c3fd15129c66ec517f240c2c260ac1
Ilya Iovva <ilichzavr@gmail.com>
2024-01-09 15:39:05 +0200

    Added simple form,simplefied validation of email and ability of sending data to server

--- index.js
+++ index.js
@@ -6,14 +6,77 @@ let formWrapper = document.getElementById("form-wrapper");
 
 let closeButton = document.getElementById("close-button");
 
+let callBackForm = document.getElementById("call-back-form");
+
+let submitFormButton = document.getElementById("submit-form");
+
+let form = document.getElementById("call-back-form");
+
+const formObject = {
+  email: form.elements.email.value,
+  name: form.elements.name.value,
+  text: form.elements.message.value,
+};
+
 formCallingButton.addEventListener("click", () => {
   formWrapper.style.display = "flex";
 
   bodyWeb.style.overflow = "hidden";
 });
 
-closeButton.addEventListener("click", () => {
+function closeForm() {
   formWrapper.style.display = "none";
 
   bodyWeb.style.overflow = "unset";
+}
+
+closeButton.addEventListener("click", closeForm);
+
+submitFormButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  let formObject = {
+    email: form.elements.email.value,
+    name: form.elements.name.value,
+    text: form.elements.message.value,
+  };
+  let emailAsArray = form.elements.email.value.split("");
+
+  if (
+    emailAsArray.includes("@") &&
+    emailAsArray.includes(".") &&
+    emailAsArray.indexOf("@") < emailAsArray.indexOf(".")
+  ) {
+    fetch("http://ip.jsontest.com", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    });
+    form.reset();
+    alert("status OK");
+  } else {
+    alert("invalid Email");
+    form.reset();
+  }
+
+  closeForm();
 });
+
+const submitForm = () => {};
+
+// function emailValidation(email) {
+//   let emailAsArray = email.split("");
+
+//   console.log(emailAsArray);
+
+//   console.log(emailAsArray.indexOf("@"));
+
+//   if (
+//     emailAsArray.includes("@") &&
+//     emailAsArray.includes(".") &&
+//     emailAsArray.indexOf("@") < emailAsArray.indexOf(".")
+//   ) {
+//     return;
+//   } else return "Введите корректный email";
+// }
